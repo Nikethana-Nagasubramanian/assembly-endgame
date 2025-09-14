7612e08675e3f01e92f6e527a2623bece62dc19e
nike935 <nike@growthfactor.ai>
2025-09-13 20:35:50 -0400

    First commit with static pages

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function AssemblyEndgame() {
+    return (
+        <main>
+            <header>
+                <h1>Assembly: Endgame</h1>
+                <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+            </header>
+            <section className="status-card">
+              <h6>You Win!</h6>
+              <p>Well done! 🎉</p>
+            </section>
+        </main>
+    )
+}
